1fb87da5ec9408c4531ff47ecca83fa28ce2ab10
Stev-G <stev.gerson@uhland44.de>
2026-03-15 08:19:26 +0100

    API Host wieder hinzugefügt

--- custom_components/vr7/const.py
+++ custom_components/vr7/const.py
@@ -9,4 +9,19 @@ DEFAULT_MARKET = "DE"
 
 SCAN_INTERVAL = 60
 
+DOMAIN = "vr7"
+
+CONF_EMAIL = "email"
+CONF_OTP = "otp"
+CONF_TOKEN = "token"
+CONF_MARKET = "market"
+
+DEFAULT_MARKET = "DE"
+
+SCAN_INTERVAL = 60
+
+# Authentifizierung (OTP Login)
 AUTH_HOST = "https://auth.ksecosys.com"
+
+# Robot API
+API_HOST = "https://beehive.ksecosys.com"
